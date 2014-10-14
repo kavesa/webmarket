2e2e226b919fa0c90d4eb98da3d24eff3d565795
nightmare <nightmare@ubuntu>
2014-10-14 03:08:42 -0200

    InfoProd

---
+++ src/java/controller/util.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.tomcat.util.codec.binary.StringUtils;
+
+/**
+ *
+ * @author nightmare
+ */
+public class util {
+    static public String byteImgToBase64(byte[] imgByte){
+        StringBuilder sb = new StringBuilder();
+            sb.append("data:image/png;base64,");
+            sb.append(StringUtils.newStringUtf8(Base64.encodeBase64(imgByte, false)));
+            String contourChart = sb.toString();
+            return contourChart;
+    }
+}
