f89375d24944d8a138722208cf045468c56c0991
kavesa <kavesa@MinK>
2014-10-16 17:05:10 -0200

    Alta Usuario con imagen en byte[]. Falta corregir check url y check campos vacios en caso cliente.

--- src/java/controller/usuario.java
+++ src/java/controller/usuario.java
@@ -7,31 +7,25 @@ package controller;
 import direct.market.enums.UsuarioType;
 import direct.market.exceptions.UsuarioException;
 import direct.market.factory.Factory;
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.*;
-import org.apache.commons.fileupload.disk.*;
-import org.apache.commons.fileupload.servlet.*;
 
 /**
  *
  * @author clobes
  */
+@MultipartConfig
 public class usuario extends HttpServlet {
 
     /**
@@ -101,111 +95,131 @@ public class usuario extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //////////////////////////////////////////////////////////////////
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-        factory.setSizeThreshold(1024);
-        String ubicacionArchivo = getServletContext().getRealPath("/") + "static/media/img/";
-        // Configure a repository (to ensure a secure temp location is used)
-        ServletContext servletContext = this.getServletConfig().getServletContext();
-        //File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
-        factory.setRepository(new File(ubicacionArchivo));
+//        DiskFileItemFactory factory = new DiskFileItemFactory();
+//        factory.setSizeThreshold(1024);
+//        String ubicacionArchivo = getServletContext().getRealPath("/") + "static/media/img/";
+//        // Configure a repository (to ensure a secure temp location is used)
+//        ServletContext servletContext = this.getServletConfig().getServletContext();
+//        //File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+//        factory.setRepository(new File(ubicacionArchivo));
+//
+//        // Create a new file upload handler
+//        ServletFileUpload upload = new ServletFileUpload(factory);
+//
+//        // Parse the request
+//        List<FileItem> items = null;
+//        try {
+//            items = upload.parseRequest(request);
+//        } catch (FileUploadException ex) {
+//            Logger.getLogger(usuario.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//
 
-        // Create a new file upload handler
-        ServletFileUpload upload = new ServletFileUpload(factory);
 
-        // Parse the request
-        List<FileItem> items = null;
+        // Process the uploaded items
+//        Iterator<FileItem> iter = items.iterator();
+
+//        while (iter.hasNext()) {
+//            FileItem item = iter.next();
+//
+//            if (item.isFormField()) {
+//                //String name = item.getFieldName();
+//                if (item.getFieldName().equals("tipo")) {
+//                    tipoForm = item.getString();
+//                }
+//                if (item.getFieldName().equals("nickname")) {
+//                    nickForm = item.getString();
+//                }
+//                if (item.getFieldName().equals("pass")) {
+//                    passForm = item.getString();
+//                }
+//                if (item.getFieldName().equals("nom")) {
+//                    nomForm = item.getString();
+//                }
+//                if (item.getFieldName().equals("ape")) {
+//                    apeForm = item.getString();
+//                }
+//                if (item.getFieldName().equals("mail")) {
+//                    mailForm = item.getString();
+//                }
+//                if (item.getFieldName().equals("fecNac")) {
+//                    fecNacForm = item.getString();
+//                }
+//                if (item.getFieldName().equals("nom-comp")) {
+//                    nomCompForm = item.getString();
+//                }
+//                if (item.getFieldName().equals("url")) {
+//                    urlForm = item.getString();
+//                }
+//            } else {
         try {
-            items = upload.parseRequest(request);
-        } catch (FileUploadException ex) {
+//                    String ext = getExtension(item.getName());
+//                    String ubicacionBd = ubicacionArchivo + nickForm + "." + ext;
+//                    if (item.getName().equals("")) {
+//                        ubicacionBd = ubicacionArchivo + "Perfil.jpg";
+//                    } else {
+//                        File file = new File(ubicacionArchivo, nickForm + "." + ext);
+//                        item.write(file);
+//                    }
+
+            ///////////////////////////////////////////////////////////////////
+
+            String tipoForm = "", nickForm, passForm, nomForm, apeForm, mailForm, fecNacForm,
+                    nomCompForm = null, urlForm = null;
+
+            nickForm = request.getParameter("nickname");
+            passForm = request.getParameter("repass");
+            nomForm = request.getParameter("nom");
+            apeForm = request.getParameter("ape");
+            mailForm = request.getParameter("mail");
+            fecNacForm = request.getParameter("fecNac");
+
+            byte[] foto = util.InputStreamToByteArray(request.getPart("imgUser").getInputStream());
+            if (foto == null || foto.length <= 0) {
+                foto = null;
+            }
+
+//                    tipoForm = "", nickForm = null, passForm = null,
+//                nomForm = null, apeForm = null, mailForm = null,
+//                fecNacForm = null, nomCompForm = null, urlForm = null;
+
+            
+            
+            //SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+            SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+            Date fechaDate = null;
+            try {
+                fechaDate = formato.parse(fecNacForm);
+            } catch (ParseException ex) {
+                Logger.getLogger(usuario.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            String tipo;
+            if (tipoForm.equals("Cliente")) {
+                tipo = UsuarioType.CLIENTE.name();
+            } else {
+                tipo = UsuarioType.PROVEEDOR.name();
+                nomCompForm = request.getParameter("nom-comp");
+                urlForm = request.getParameter("url");
+            }
+            //impacta en bd
+            //Factory.getInstance().getUsuarioController().altaUsuario(nickForm, passForm, nomForm, apeForm, fechaDate, mailForm, ubicacionBd, tipo, nomCompForm, urlForm);
+            Factory.getInstance().getUsuarioController().altaUsuario(nickForm, passForm, nomForm, apeForm, fechaDate, mailForm, foto, tipo, nomCompForm, urlForm);
+            //mensaje de success y redirige a usuario
+            request.getSession().setAttribute("success", "Usuario Creado Correctamente.");
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/vistas/usuario/usuario.jsp");
+            dispatcher.forward(request, response);
+
+        } catch (UsuarioException ex) {
+            //si ocurre alguna excepcion larga un msg
+            request.getSession().setAttribute("error", ex.getMessage());
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/vistas/usuario/usuario.jsp");
+            dispatcher.forward(request, response);
+        } catch (Exception ex) {
             Logger.getLogger(usuario.class.getName()).log(Level.SEVERE, null, ex);
         }
 
 
-        
-        
-        // Process the uploaded items
-        Iterator<FileItem> iter = items.iterator();
-        String tipoForm = "", nickForm = null, passForm = null,
-                nomForm = null, apeForm = null, mailForm = null,
-                fecNacForm = null, nomCompForm = null, urlForm = null;
-
-        while (iter.hasNext()) {
-            FileItem item = iter.next();
-
-            if (item.isFormField()) {
-                //String name = item.getFieldName();
-                if (item.getFieldName().equals("tipo")) {
-                    tipoForm = item.getString();
-                }
-                if (item.getFieldName().equals("nickname")) {
-                    nickForm = item.getString();
-                }
-                if (item.getFieldName().equals("pass")) {
-                    passForm = item.getString();
-                }
-                if (item.getFieldName().equals("nom")) {
-                    nomForm = item.getString();
-                }
-                if (item.getFieldName().equals("ape")) {
-                    apeForm = item.getString();
-                }
-                if (item.getFieldName().equals("mail")) {
-                    mailForm = item.getString();
-                }
-                if (item.getFieldName().equals("fecNac")) {
-                    fecNacForm = item.getString();
-                }
-                if (item.getFieldName().equals("nom-comp")) {
-                    nomCompForm = item.getString();
-                }
-                if (item.getFieldName().equals("url")) {
-                    urlForm = item.getString();
-                }
-            } else {
-                try {
-                    String ext = getExtension(item.getName());
-                    String ubicacionBd = ubicacionArchivo + nickForm + "." + ext;
-                    if (item.getName().equals("")) {
-                        ubicacionBd = ubicacionArchivo + "Perfil.jpg";
-                    } else {
-                        File file = new File(ubicacionArchivo, nickForm + "." + ext);
-                        item.write(file);
-                    }
-
-                    ///////////////////////////////////////////////////////////////////
-                    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-                    Date fechaDate = null;
-                    try {
-                        fechaDate = formato.parse(fecNacForm);
-                    } catch (ParseException ex) {
-                        Logger.getLogger(usuario.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    String tipo;
-                    if (tipoForm.equals("Cliente")) {
-                        tipo = UsuarioType.CLIENTE.name();
-                    } else {
-                        tipo = UsuarioType.PROVEEDOR.name();
-                    }
-                    //impacta en bd
-                    //Factory.getInstance().getUsuarioController().altaUsuario(nickForm, passForm, nomForm, apeForm, fechaDate, mailForm, ubicacionBd, tipo, nomCompForm, urlForm);
-                    Factory.getInstance().getUsuarioController().altaUsuario(nickForm, passForm, nomForm, apeForm, fechaDate, mailForm, null, tipo, nomCompForm, urlForm);
-                    //mensaje de success y redirige a usuario
-                    request.getSession().setAttribute("success", "Usuario Creado Correctamente.");
-                    RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/vistas/usuario/usuario.jsp");
-                    dispatcher.forward(request, response);
-
-                } catch (UsuarioException ex) {
-                    //si ocurre alguna excepcion larga un msg
-                    request.getSession().setAttribute("error", ex.getMessage());
-                    RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/vistas/usuario/usuario.jsp");
-                    dispatcher.forward(request, response);
-                } catch (Exception ex) {
-                    Logger.getLogger(usuario.class.getName()).log(Level.SEVERE, null, ex);
-                }
-
-
-            }
-        }
     }
 
     /**
