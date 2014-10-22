5a730c73046647fa020a11495deace0640e96da8
kavesa <kavesa@MinK>
2014-10-21 23:21:57 -0200

    servlet buscador de productos con nombre similar o proveedor similar a string de entrada

--- src/java/controller/buscador.java
+++ src/java/controller/buscador.java
@@ -13,10 +13,12 @@ import java.io.PrintWriter;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -39,7 +41,7 @@ public class buscador extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            String stringBuscado = (String) request.getParameter("a_buscar");
+            String stringBuscado = ((String) request.getParameter("a_buscar")).trim();
             if (stringBuscado.length() < 3) {
                 request.setAttribute("error", "La palabra buscada debe tener al menos 3 letras");
                 //request.getRequestDispatcher("/vistas/producto/ProductNavigation.jsp").forward(request, response);
@@ -49,14 +51,32 @@ public class buscador extends HttpServlet {
 
                 List<DataProducto> catResult = Factory.getInstance().getCategoriaController().getProductosPorNombreCategoria(stringBuscado);
                 //itero para no agregar un producto dos veces (que ya haya sido agregado por similitud)
-                for (DataProducto dp : catResult) {
-                    for(DataProducto result : listResult){
-                        if(!dp.getReferencia().equals(result.getReferencia())){
+                if (catResult != null && !catResult.isEmpty()) {
+                    for (DataProducto dp : catResult) {
+                        boolean yaesta = false;
+                        for (DataProducto result : listResult) {
+                            if (dp.getNombre().equals(result.getNombre())) {
+                                yaesta = true;
+                            }
+                        }
+                        if (!yaesta) {
                             listResult.add(dp);
                         }
                     }
                 }
-                request.setAttribute("resultado", listResult);
+                if (listResult == null || listResult.isEmpty()) {
+                    JSONObject noencontrado = new JSONObject();
+                    noencontrado.put("result_search", false);
+                    out.print(noencontrado);
+                } else {
+
+                    request.setAttribute("result_search", listResult);
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("/vistas/producto/SearchProductos.jsp");
+                    dispatcher.forward(request, response);
+//                    request.setAttribute("result_search", listResult);
+//                    RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/vistas/producto/SearchProductos.jsp");
+//                    dispatcher.forward(request, response);
+                }
             }
         } finally {
             out.close();
